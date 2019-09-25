363dc01815a893736a99a94b46b333bf505c8207
fernandosenchuk <55700969+fernandosenchuk@users.noreply.github.com>
2019-09-25 20:03:12 -0300

    Proyecto Lands. Pantalla LoginPage.

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lands.ViewModels;
+
+namespace Lands.Infrastructure
+{
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
