02015badb01cfd90d1c20a93e660278f17695a3e
fernandosenchuk <55700969+fernandosenchuk@users.noreply.github.com>
2019-09-29 12:31:58 -0300

    Check Connection

--- Lands/Lands/ViewModels/LandsViewModel.cs
+++ Lands/Lands/ViewModels/LandsViewModel.cs
@@ -1,10 +1,79 @@
-﻿using System;
+﻿using Lands.Models;
+using Lands.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace Lands.ViewModels
 {
-    public class LandsViewModel
+    public class LandsViewModel :BaseViewModel
     {
+        #region Services
+
+        private ApiService apiService;
+
+        #endregion
+
+        #region Attributes
+
+        private ObservableCollection<Land> lands;
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<Land> Lands
+        {
+            get { return lands; }
+            set { SetValue(ref lands, value); }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public LandsViewModel()
+        {
+            this.apiService = new ApiService();
+
+            this.LoadLands();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private async void LoadLands()
+        {
+            var connection = await this.apiService.CheckConnection();
+
+            if (!connection.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", connection.Message, "Accept");
+
+                await Application.Current.MainPage.Navigation.PopAsync();
+
+                return;
+            }
+
+            var response = await this.apiService.GetList<Land>("http://restcountries.eu", "/rest", "/v2/all");
+
+            if (response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+
+                await Application.Current.MainPage.Navigation.PopAsync();
+
+                return;
+            }
+
+            var list = (List<Land>)response.Result;
+
+            this.Lands = new ObservableCollection<Land>(list);
+        }
+
+        #endregion
     }
 }
